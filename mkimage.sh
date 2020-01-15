a9088cda31d2b0f7db1b603f58924e5475d27d51
root <root@localhost.localdomain>
2020-01-15 15:32:06 +0800

    add mkimage.sh file

---
+++ mkimage.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+repopath="http://192.168.39.230/centos8/isoft/RPMS/"
+sqlite="d4ac5c1e438377b1a701dbf086fe291d2c3007c3ef2d275f33e3824564abb994-primary.sqlite"
+fsdir=rootfs-$(date +%s)
+env_check()
+{
+    cmd_list="rpm2cpio wget tar docker sqlite3 bunzip2 git" 
+    for cmd in ${cmd_list[@]}; do
+	    which "$cmd" >/dev/null 2>&1
+	    if [ $? -ne 0 ]; then
+            echo -e "\033[5;31m $cmd \033[0m"
+            echo -e "\033[4;31m $cmd Command does not exist, please install this command\033[0m"
+            exit
+        fi
+    done
+}
+down_pkg()
+{
+#    git clone https://github.com/zhuyaliang/mkimage
+    rm -rf org_env
+    mkdir org_env 
+    cd org_env
+    cat ../mkimage/pkg.list |while read pkg
+    do
+        pkgpath=$(sqlite3 ../$sqlite "select location_href from packages where name='$pkg'")
+        if [ -n "$pkgpath" ]; then
+            echo $pkgpath
+            wget $repopath$pkgpath
+        fi
+    done
+}
+unpacking_rpm()
+{
+    for pkg in $(ls);
+    do
+        rpm2cpio $pkg | cpio -div
+    done
+    /usr/bin/cp -rfa lib64/* usr/lib64/
+    rm -rf lib64
+    ln -s /usr/lib64/ lib64 
+    cd ../
+}
+create_rootfs_env()
+{
+    list="bash ls microdnf"
+    mkdir -p $fsdir/bin $fsdir/lib64 $fsdir/usr/ $fsdir/usr/bin/ $fsdir/usr/lib/rpm  $fsdir/usr/lib64 $fsdir/etc/pki $fsdir/proc
+    for i in $list
+    do
+        cp -raf "mkimage/"$i "${fsdir}/usr/bin/"
+    done
+    mkdir -p $fsdir/lib/x86_64-linux-gnu/
+    
+    cat ./mkimage/lib.list |while read line
+    do
+        lib="org_env/"$line
+        link="$(readlink $lib -f)"
+        cp -rfa "$lib" "${fsdir}${line}"
+        cp -rfa "$link" "${fsdir}/lib64/"
+        cp -rfa "$link" "${fsdir}/usr/lib64/"
+    done
+    
+    cp -rf org_env/etc/os-release  $fsdir/etc/.
+    cp -rf org_env/usr/lib/rpm/rpmrc $fsdir/usr/lib/rpm/
+    cp -rf org_env/usr/lib/rpm/macros $fsdir/usr/lib/rpm/
+    cp -rf org_env/etc/pki/rpm-gpg $fsdir/etc/pki/
+
+    /usr/bin/cp -rf $fsdir/lib64/* $fsdir/usr/lib64/
+    /usr/bin/cp -rf $fsdir/lib/* $fsdir/usr/lib/
+
+    rm -rf $fsdir/lib64/
+    rm -rf $fsdir/bin/
+    rm -rf $fsdir/lib/
+    cd $fsdir
+    ln -s usr/bin bin
+    ln -s usr/lib64 lib64
+    ln -s usr/lib lib
+
+}
+create_image_repo()
+{
+    mkdir etc/yum.repos.d/ -p
+    touch etc/yum.repos.d/image_aarch64.repo
+    echo [openEuler] >> etc/yum.repos.d/image_aarch64.repo 
+    echo name=openEuler >> etc/yum.repos.d/image_aarch64.repo
+    echo baseurl=$repopath >> etc/yum.repos.d/image_aarch64.repo
+    echo gpgcheck=0 >> etc/yum.repos.d/image_aarch64.repo
+    echo gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-openEuler >> etc/yum.repos.d/image_aarch64.repo
+    echo priority=1 >> etc/yum.repos.d/image_aarch64.repo
+    cd ../
+}
+create_docker_image()
+{
+    sudo tar -C $fsdir -c . | sudo docker import - new_aarch64_os:latest
+}
+#main
+
+env_check
+down_pkg
+unpacking_rpm
+create_rootfs_env
+create_image_repo
+create_docker_image
