12a9c1f7dc456d6f0b6112593a5ec07ff095ec17
root <root@localhost.localdomain>
2020-01-15 15:06:49 +0800

    update pkg.list

--- mkpkglist.sh
+++ mkpkglist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf *.list
 list="microdnf bash"
 for i in $list
 do
@@ -16,25 +17,3 @@ echo openEuler-gpg-keys >> ./tmp.list
 echo openEuler-release >> ./tmp.list
 cat ./tmp.list | sort -k2n | uniq > ./pkg.list
 rm -rf ./tmp.list
-#cp -rf /usr/lib64/liblua-5.3.so $dir/usr/lib64/
-#cp -rf /usr/lib64/libdb-5.3.so $dir/usr/lib64/
-#cp -rf /usr/lib64/libsecurec.so $dir/usr/lib64/
-#
-#cp -rf /etc/os-release  $dir/etc/.
-#cp -rf /etc/yum.repos.d/ $dir/etc/
-#cp -rf /usr/lib/rpm/rpmrc $dir/usr/lib/rpm/
-#cp -rf /usr/lib/rpm/macros $dir/usr/lib/rpm/
-#cp -rf /etc/pki/rpm-gpg $dir/etc/pki/
-#
-#/usr/bin/cp -rf $dir/lib64/* $dir/usr/lib64/
-#/usr/bin/cp -rf $dir/lib/* $dir/usr/lib/
-#
-#rm -rf $dir/lib64/
-#rm -rf $dir/bin/
-#rm -rf $dir/lib/
-#cd $dir
-#ln -s usr/bin bin
-#ln -s usr/lib64 lib64
-#ln -s usr/lib lib
-#mount --bind /proc/ proc/
-#chroot .
